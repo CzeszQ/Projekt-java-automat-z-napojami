aecaca37f583d85e030857828ed089fdbd596735
dejwid cz <example@.com>
2024-06-21 17:39:52 +0200

    welcome form update

--- src/Welcome_form.java
+++ src/Welcome_form.java
@@ -1,2 +1,33 @@
-public class Welcome_form {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Welcome_form extends JFrame {
+    private JPanel panel1;
+    private JTextField WITAMYWAUTOMACIEZTextField;
+    private JButton WelcomeButton;
+
+
+    int width = 800, height = 800;
+
+
+
+    public Welcome_form(){
+        super();
+        this.setContentPane(this.panel1);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+        this.setSize(width, height);
+
+
+        WelcomeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+
+
+
 }

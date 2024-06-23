05f1608453d2765739a7d84936da4b48e90aa35b
dejwid cz <example@.com>
2024-06-23 14:22:34 +0200

    saldo container

---
+++ src/SaldoContainer.java
@@ -0,0 +1,15 @@
+public class SaldoContainer {
+        private double saldo;
+
+        public SaldoContainer(double saldo) {
+            this.saldo = saldo;
+        }
+
+        public double getSaldo() {
+            return saldo;
+        }
+
+        public void setSaldo(double saldo) {
+            this.saldo = saldo;
+        }
+}
